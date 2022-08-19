09c1548d9419114b05e88a69c9cd038a76bfe797
santiagodata <santiago.data.ing@gmail.com>
2022-08-18 22:55:38 -0500

    version 1.3.0

--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -7,16 +7,25 @@ import {Observable} from "rxjs";
 })
 export class FlightsService {
 
-  public API_URL = "https://recruiting-api.newshore.es/api/flights/0/"
+  public API_URL = "https://recruiting-api.newshore.es/api/flights/0/" //URL de conexion a la API
 
+  // Contructor
   constructor(public http:HttpClient) { }
 
+  // Obtener Vuelos
  getFlights():Observable<any>
   {
     return this.http.get(this.API_URL);
   }
 
+  // Obtener origenes
   getOrigin(searchTerm:string):Observable<any>
+  {
+    return this.http.get(this.API_URL+"?"+searchTerm);
+  }
+
+  // Obtener destinos
+  getDestination(searchTerm:string):Observable<any>
   {
     return this.http.get(this.API_URL+"?"+searchTerm);
   }
